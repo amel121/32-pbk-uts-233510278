00251065654859bea2d89f2dc33bae8ce17a9260
amel121 <amelianeviani185@gmail.com>
2025-05-07 13:55:16 +0700

    penambahan button resetAllActivities (reset semua)

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,10 @@ const removeActivity = (index) => {
   activities.value.splice(index, 1)
 }
 
+const resetAllActivities = () => {
+  activities.value = []
+}
+
 const filteredActivities = computed(() => {
   if (filterOption.value === 'done') {
     return activities.value.filter((a) => a.done)
@@ -26,24 +30,24 @@ const filteredActivities = computed(() => {
 })
 </script>
 
-
 <template>
   <div class="container">
     <h1>Daftar Kegiatan</h1>
 
     <form class="form-group" @submit.prevent="addActivity">
-  <input v-model="newActivity" placeholder="Tambah kegiatan baru" />
-  <button type="submit">Tambah</button>
-</form>
+      <input v-model="newActivity" placeholder="Tambah kegiatan baru" />
+      <button type="submit">Tambah</button>
+      <button type="button" @click="resetAllActivities">Reset Semua</button>
+    </form>
 
     <div class="filters">
-  <label for="filter">Filter Kegiatan:</label>
-  <select id="filter" v-model="filterOption">
-    <option value="all">Semua pilihan Tampilkan</option>
-    <option value="not_done">Tampilkan hanya yang belum selesai</option>
-    <option value="done">Tampilkan hanya yang sudah selesai</option>
-  </select>
-</div>
+      <label for="filter">Filter Kegiatan:</label>
+      <select id="filter" v-model="filterOption">
+        <option value="all">Semua pilihan Tampilkan</option>
+        <option value="not_done">Tampilkan hanya yang belum selesai</option>
+        <option value="done">Tampilkan hanya yang sudah selesai</option>
+      </select>
+    </div>
 
     <ul>
       <li v-for="(activity, index) in filteredActivities" :key="index">
@@ -54,5 +58,3 @@ const filteredActivities = computed(() => {
     </ul>
   </div>
 </template>
-
-
